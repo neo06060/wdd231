fd710788c37381d53fa0cedb8e033c3e0e1e78cb
neo06060 <saulcgaos@gmail.com>
2025-05-08 15:10:37 +0200

    a

--- scripts/scripts-navigation.js
+++ scripts/scripts-navigation.js
@@ -1,12 +1,14 @@
-const menuBtn = document.getElementById("menu");
-const navList = document.querySelector(".navigation");
+document.addEventListener("DOMContentLoaded", () => {
+  const menuBtn = document.getElementById("menu");
+  const navList = document.querySelector("nav ul");
 
-menuBtn.addEventListener("click", () => {
-  navList.classList.toggle("open");
-});
+  menuBtn.addEventListener("click", () => {
+    navList.classList.toggle("open");
+  });
 
-document.querySelectorAll(".navigation a").forEach(link => {
-  link.addEventListener("click", () => {
-    navList.classList.remove("open");
+  document.querySelectorAll(".navigation a").forEach(link => {
+    link.addEventListener("click", () => {
+      navList.classList.remove("open");
+    });
   });
 });
