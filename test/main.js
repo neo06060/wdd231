8bdc4962bfdc02dd8513c53f484c550670c2a499
neo06060 <saulcgaos@gmail.com>
2025-06-13 18:36:57 +0200

    a

---
+++ test/main.js
@@ -0,0 +1,7 @@
+// Mobile nav toggle
+const burger = document.getElementById('burger');
+const nav = document.getElementById('nav');
+
+burger.addEventListener('click', () => {
+  nav.classList.toggle('open');
+});
